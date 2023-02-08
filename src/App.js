cb5509e58cbd69e0330f8af8dd65a1f4cf48fe36
Jose Hernandez <jhernandez_456@yahoo.com>
2023-02-07 21:55:00 -0800

    upload to git repo

--- src/App.js
+++ src/App.js
@@ -1,25 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react"
+import "./App.css"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+    state = {
+        numbers: {
+            diceOne: 0,
+            diceTwo: 0,
+            diceThree: 0,
+            diceFour: 0,
+            diceFive: 0,
+            diceSix: 0
+        }
+    }
+
+    random = () => Math.floor(Math.random() * 6) + 1
+
+    rollDice = () => this.setState({
+        numbers: {
+            diceOne: this.random(),
+            diceTwo: this.random(),
+            diceThree: this.random(),
+            diceFour: this.random(),
+            diceFive: this.random(),
+            diceSix: this.random()
+        }
+    })
+
+
+    render() {
+        return (
+            <div>
+                <h1>D I C E</h1>
+                <div className="box">
+                    <h1>{this.state.numbers.diceOne}</h1>
+                    <h1>{this.state.numbers.diceTwo}</h1>
+                    <h1>{this.state.numbers.diceThree}</h1>
+                    <h1>{this.state.numbers.diceFour}</h1>
+                    <h1>{this.state.numbers.diceFive}</h1>
+                    <h1>{this.state.numbers.diceSix}</h1>
+                </div>
+                <button className="dice" onClick={this.rollDice}>R O L L</button>
+
+            </div>
+        )
+    }
 }
+export default App
 
-export default App;
